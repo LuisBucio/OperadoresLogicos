488918417e03fb49f6683fcd0af86b6c2095d52f
Luis Bucio <luis.mtz.bucio@gmail.com>
2021-07-15 22:32:28 -0500

    Primer commit

---
+++ OperadoresLogicos/src/OperadoresLogicos.java
@@ -0,0 +1,21 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*OPERADORES LOGICOS
+		 * a == b => a es igual a b
+		 * a > b => a es mayor a b
+		 * a < b => a es menor a b
+		 * a != b => a es diferente a b*/
+		
+		System.out.println(saludo());
+
+	}
+
+public static String saludo() {
+	return "Saludo";
+}
+
+}
